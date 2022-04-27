aae90f173b7c25c781c42daa8c102b76e7c6bd52
Ritta <brittanykelly33@gmail.com>
2022-04-26 21:29:10 -0400

    commit tree 0930b32e710d2e1dd07e057e3081e3f2f4568485

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCatLast = [...cats, name];
+    return newCatLast;
+}
+
+function prependCat(name) {
+    const newCatFirst = [name, ...cats];
+    return newCatFirst;
+}
+
+function removeLastCat() {
+    const byeLastCat = cats.slice(0,2);
+    return byeLastCat;
+}
+
+function removeFirstCat() {
+    const byeFirstCat = cats.slice(1);
+    return byeFirstCat;
+}
